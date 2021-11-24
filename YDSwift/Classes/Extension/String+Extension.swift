e886e117e05137d15e08906fb0ef72772605e3f9
gaoyuan <707216098@qq.com>
2021-11-24 11:15:41 +0800

    add

---
+++ YDSwift/Classes/Extension/String+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  String.swift
+//  YDSwift
+//
+//  Created by gaoyuan on 2021/11/24.
+//
+
+import UIKit
+
+extension String {
+    
+    /// 截取字符串
+    /// - Parameters:
+    ///   - from: 开始的索引
+    ///   - to: 结束的索引
+    public func subString(from: Int, to: Int) -> String {
+        let start = index(startIndex, offsetBy: from)
+        let end = index(startIndex, offsetBy: to)
+        let result = self[start..<end]
+        return String(result)
+    }
+}
